30156b27831686300c9adc48739a2132bc0d9daf
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-15 15:59:57 -0700

    Header_final

--- main.h
+++ main.h
@@ -16,12 +16,12 @@
 struct convert
 {
 	char *sym;
-	int(*f)(va_list);
+	int (*f)(va_list);
 };
 typedef struct convert conver_t;
 
 /*Main functions*/
-int parser(const char *format, conver_t f_list[],va_list arg_list);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
 int print_char(va_list);
