825d094f983c021999025f0ba08c335c21d06ae5
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-15 12:08:40 -0700

    updated header

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <stdarg.h>
@@ -11,7 +11,7 @@
  * struct convert - defines a structure for symbols and functions
  *
  * @sym: The operator
- * @f: The function associ
+ * @f: The function associations
  */
 struct convert
 {
@@ -36,11 +36,12 @@ int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
+int _putchar(char c);
+int _puts(char *str);
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
+int print_unsigned_number(unsigned int);
 #endif
-
