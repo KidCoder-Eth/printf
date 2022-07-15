ce9f5c114c9e9011d8adb6789d2eeeed4b8d9895
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-15 12:40:09 -0700

    updated print_nums

--- 1_print_nums.c
+++ 1_print_nums.c
@@ -1,85 +1,61 @@
 #include "main.h"
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_unsigned_number - prints an unsigned integer
+ * @n: unsigned integer to be printed
+ * Return: numbers printed
  */
-int print_int(va_list l, flags_t *f)
+int print_unsigned_number(unsigned int n)
 {
-	int n = va_arg(l, int);
-	int res = count_digit(n);
+	int div;
+	int len;
+	unsigned int num;
 
-	if (f->space == 1 && f->plus == 0 && n >= 0)
-		res += _putchar(' ');
-	if (f->plus == 1 && n >= 0)
-		res += _putchar('+');
-	if (n <= 0)
-		res++;
-	print_number(n);
-	return (res);
-}
-/**
- * print_unsigned - prints an unsigned integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
- */
-int print_unsigned(va_list l, flags_t *f)
-{
-	unsigned int u = va_arg(l, unsigned int);
-	char *str = convert(u, 10, 0);
+	div = 1;
+	len = 0;
 
-	(void)f;
-	return (_puts(str));
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (len);
 }
 /**
  * print_number - helper function that loops through
- * an integer and prints all its digits
- * @n: integer to be printed
+ * an integer and prints
+ * @args: List of arguments
+ * Return: The number of arguments printed
  */
-void print_number(int n)
+int print_number(va_list args)
 {
-	unsigned int n1;
+	int n;
+	int div;
+	int len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		len += _putchar('-');
+		num = n * -1;
 	}
 	else
-		n1 = n;
-	if (n1 / 10)
-		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
-}
-/**
- * count_digit - returns the number of digits in an integer
- * for _printf
- * @i: integer to evaluate
- * Return: number of digits
- */
-int count_digit(int i)
-{
-	unsigned int d = 0;
-	unsigned int u;
-
-	if (i < 0)
-		u = i * -1;
-	else
-		u = i;
-	while (u != 0)
+		num = n;
+	for (; num / div > 9; )
+		div *= 10;
+	for (; div != 0; )
 	{
-		u /= 10;
-		d++;
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
 	}
-	return (d);
-<<<<<<< HEAD
-}	
-=======
+	return (len);
 }
-
-
->>>>>>> 0033738feafa058342ac21e5ffede020cf4a3144
