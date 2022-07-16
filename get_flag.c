daa52be29eb7658bbd915992ede57c62c3552b91
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-07-15 17:32:15 -0700

    get_flag

---
+++ get_flag.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+/**
+ * get_flag - turns on flags if _printf finds a flag
+ * modifier in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: 1 if a flag has been turned on, 0 otherwise
+ */
+int get_flag(char s, flags_t *f)
+{
+	int i = 0;
+
+	switch (s)
+	{
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
+			break;
+	}
+	return (i);
+}
